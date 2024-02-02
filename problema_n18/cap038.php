1e9fa0f5f1b0e2c083699169f6239817e2723c6e
jesus <jesus_alejandro_123@outlook.com>
2024-02-01 20:00:27 -0600

    nuevos_ejercicios

--- problema_n18/cap038.php
+++ problema_n18/cap038.php
@@ -1,17 +1,69 @@
 <?php
+// Declaración de variable 
+// Dado que se tiene 3 números, devolver los números en orden ascendente.
+$num1 = 0 ; $num2 = 0 ; $num3 = 0 ; $may = 0 ; $inter = 0 ; $men = 0 ;
 
+if (isset($_POST["btnCalcular"])){
+    // Declaración de las entradas
+    $num1 = (int) $_POST["txtnum1"];
+    $num2 = (int) $_POST["txtnum2"];
+    $num3 = (int) $_POST["txtnum3"];
 
+    if($num1>$num2){
+        if($num1>$num3){
+            if($num2>$num3){
+                $may = $num1;
+                $inter = $num2;
+                $men = $num3;
+            }else{
+                $may = $num1;
+                $inter = $num3;
+                $men = $num2;
+            }
+        }else{
+            if($num1>$num2){
+                $may = $num3;
+                $inter = $num1;
+                $men = $num2;
+            }else{
+                $may = $num3;
+                $inter = $num2;
+                $men = $num1;
+            }
 
-
-
-
+        }
+    }else{
+        if($num2>$num3){
+            if($num1>$num3){
+                $may = $num2;
+                $inter = $num1;
+                $men = $num3;
+            }else{
+                $may = $num2;
+                $inter = $num3;
+                $men = $num1;
+            }
+        }else{
+            if($num1>$num2){
+                $may = $num3;
+                $inter = $num1;
+                $men = $num2;
+            }else{
+                $may = $num3;
+                $inter = $num2;
+                $men = $num1;
+            }
+        }
+    }
+    
+}
 
 
 ?>
 
 <html>
     <head>
-    <title>Problema X</title>
+    <title>Problema 16</title>
     <style style type="text/css">
     <!--
     .TextoFondo {
@@ -21,22 +73,47 @@
     </style>
     </head>
     <body>
-    <form method="post" action="cap035.php">
+    <form method="post" action="cap038.php">
     <table width="237" border="0">
-    <h1>Determinar si un numero entero es par o impar</h1>
+    <p>Dados tres numeros enteros, devolver los números en orden ascendentes</p>
         <tr>
-            <td colspan="2"><strong>Problema 15</strong></td>
+            <td colspan="2"><strong>Problema 18</strong></td>
         </tr>
         <tr>
-            <td width="83">Numero</td>
+            <td width="83">Numero_1</td>
             <td>
-                <input name="txtn" type="text"  id="txtn" value="<?=$n?>"/>
+                <input name="txtnum1" type="text"  id="txtnum1" value="<?=$num1?>"/>
+            </td>
+        </tr>
+        <tr>
+            <td width="83">Numero_2</td>
+            <td>
+                <input name="txtnum2" type="text"  id="txtnum2" value="<?=$num2?>"/>
+            </td>
+        </tr>
+        <tr>
+            <td width="83">Numero_3</td>
+            <td>
+                <input name="txtnum3" type="text"  id="txtnum3" value="<?=$num3?>"/>
             </td>
         </tr>
          <tr>
-            <td width="83">Resultado</td>
+            <td width="83">Mayor</td>
             <td>
-                <input name="txtr" type="text"  id="txtr" value="<?=$r?>"/>
+                <input name="txtmay" type="text"  id="txtmay" value="<?=$may?>" class="TextoFondo" />
+            </td>
+        </tr>
+        <tr>
+            <td width="83">Intermedio</td>
+            <td>
+                <input name="txtinter" type="text"  id="txtinter" value="<?=$inter?>" class="TextoFondo" />
+            </td>
+        </tr>
+        <tr>
+        <tr>
+            <td width="83">Medio</td>
+            <td>
+                <input name="txtmen" type="text"  id="txtmen" value="<?=$men?>" class="TextoFondo" />
             </td>
         </tr>
         <tr>
--- problema_n19/cap039.php
+++ problema_n19/cap039.php
@@ -1,17 +1,37 @@
 <?php
+// Un restaurante ofrece un descuento del 10% para consumos de hasta s/.1000; y un descuento del 20% para consumos
+// mayores. En ambos casos se aplica un impuesto del 19%. 
+// Determinar el monto del descuento, el impuesto y el importe a pagar.
 
+// costantes
 
+define("D1",0.1);
+define("D2",0.2);
+define("IGV",0.19);
 
+// Declaración de variables
+$csumo = 0 ; $m_d = 0 ; $imp = 0 ; $imp_apar = 0 ;
+if (isset($_POST["btnCalcular"])){
+    // Entrada
+    $csumo = (float) $_POST["txtcsumo"];
 
+    // proceso
+    if($csumo <=100){
+        $m_d = $csumo * D1 ;
+    }else{
+        $m_d = $csumo * D2 ;
+    }
 
+    $imp = ($csumo-$m_d) * IGV;
+    $imp_apar = $csumo - $m_d + $imp ;
 
-
+}
 
 ?>
 
 <html>
     <head>
-    <title>Problema X</title>
+    <title>Problema 19</title>
     <style style type="text/css">
     <!--
     .TextoFondo {
@@ -21,22 +41,34 @@
     </style>
     </head>
     <body>
-    <form method="post" action="cap035.php">
+    <form method="post" action="cap039.php">
     <table width="237" border="0">
-    <h1>Determinar si un numero entero es par o impar</h1>
+    <p>Determinar el monto del descuento, el impuesto y el importe a pagar.</p>
         <tr>
-            <td colspan="2"><strong>Problema 15</strong></td>
+            <td colspan="2"><strong>Problema 19</strong></td>
         </tr>
         <tr>
-            <td width="83">Numero</td>
+            <td width="83">Consumo</td>
             <td>
-                <input name="txtn" type="text"  id="txtn" value="<?=$n?>"/>
+                <input name="txtcsumo" type="text"  id="txtcsumo" value="<?=$csumo?>"/>
             </td>
         </tr>
          <tr>
-            <td width="83">Resultado</td>
+            <td width="83">Monto_descuento</td>
             <td>
-                <input name="txtr" type="text"  id="txtr" value="<?=$r?>"/>
+                <input name="txtm_d" type="text"  id="txtm_d" value="<?=$m_d?>" class="TextoFondo"/>
+            </td>
+        </tr>
+        <tr>
+            <td width="83">Impuesto_IGV</td>
+            <td>
+                <input name="txtimp" type="text"  id="txtimp" value="<?=$imp?>" class="TextoFondo"/>
+            </td>
+        </tr>
+        <tr>
+            <td width="83">Importe_a_pagar</td>
+            <td>
+                <input name="txtimp_apar" type="text"  id="txtimp_apar" value="<?=$imp_apar?>" class="TextoFondo"/>
             </td>
         </tr>
         <tr>
