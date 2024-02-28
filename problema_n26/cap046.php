0b6e83a6504028b0692677ef01db917ab9448473
jesus <jesus_alejandro_123@outlook.com>
2024-02-27 21:02:08 -0600

    problema 26 php

--- problema_n26/cap046.php
+++ problema_n26/cap046.php
@@ -1,25 +1,26 @@
 <?php
-// Elabore un algoritmo que obtenga las raices reales de una ecuación de segundo grado. 
 // Declaración de variables
-
-$a = "" ; $b = "" ; $c = "" ; $x1 = 0 ; $x2 = 0 ; 
-
+$num = "" ; $estacion = "" ;
 
 if (isset($_POST["btnCalcular"])){
-    // Entradas
-$a = (float) $_POST["txta"];
-$b = (float) $_POST["txtb"];
-$c = (float) $_POST["txtc"];
+    $num = (int) $_POST["txtnum"];
+    switch($num){
+        case 1:
+            $estacion = "Verano";
+            break;
+        case 2:
+            $estacion = "Otoño";
+            break;
+        case 3:
+            $estacion = "Invierno";
+            break;
+        case 4:
+            $estacion = "Primavera";
+            break;
+    }
 
-if($a != 0){
-    $x1 = (-$b + pow((pow($b,2)- 4*$a*$c),1/2))/2*$a;
-    $x2 = (-$b - pow((pow($b,2)- 4*$a*$c),1/2))/2*$a;
-}
-else{
-    echo "Advertencia: Ingrese un valor a diferente de cero ";
 }
 
-}
 
 ?>
 
@@ -28,63 +29,56 @@ else{
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Problema 24</title>
-    <style>
-    .TextoFondo {
-        background-color: #CCFFFF;
-    }
-
-
-    #txta{
-        background-color: rgb(188, 167, 229);
-    }
- 
-    #txtb{
-        background-color: rgb(188, 167, 229);
-    }
- 
-    #txtc{
-        background-color: rgb(188, 167, 229);
-    }
-
-    </style>
+    <title>Problema 26</title>
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-<form method="post" action="cap046.php">
+<h1>Problema 26</h1>
+   
+<p>
+    Al Ingresar un número entre 1 y 4, devolver la estación del año de acuerdo a la siguiente tabla.
+</p>
+
+<table class="tabla-estaciones">
+    <tr class="fondo-fila">
+        <td>Número</td>
+        <td>Estación</td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Verano</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Otoño</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Invierno</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>Primavera</td>
+    </tr>
+</table>
+
+<br>
+
+    <form method="post" action="cap046.php">
     <table width="500">
-    <h1>Problema 24</h1>
-    <p>Interfaz para calcular las raices reales de una ecuación de segundo grado.</p>
-        <tr>
-            <td colspan="2"><strong>Problema 24</strong></td>
+     <tr>
+            <td colspan="2"><strong>Problema 26</strong></td>
         </tr>
         <tr>
-            <td width="83">valor a</td>
+            <td width="83">Número</td>
             <td>
-                <input name="txta" type="text"  id="txta" value="<?=$a?>"/>
-            </td>
-        </tr>
-         <tr>
-            <td width="83">valor b</td>
-            <td>
-                <input name="txtb" type="text"  id="txtb" value="<?=$b?>" />
+                <input name="txtnum" type="text"  id="txtnum" value="<?=$num?>"/>
             </td>
         </tr>
         <tr>
-            <td width="83">valor c</td>
+            <td width="83">Estación</td>
             <td>
-                <input name="txtc" type="text"  id="txtc" value="<?=$c?>" />
-            </td>
-        </tr>
-        <tr>
-            <td width="83">x1</td>
-            <td>
-                <input name="txtx1" type="text"  id="txtx1" value="<?=$x1?>" class="TextoFondo"/>
-            </td>
-        </tr>
-        <tr>
-            <td width="83">x2</td>
-            <td>
-                <input name="txtx2" type="text"  id="txtx2" value="<?=$x2?>" class="TextoFondo"/>
+                <input name="txtestacion" type="text"  id="txtestacion" value="<?=$estacion?>" class="fondo-resultado"/>
             </td>
         </tr>
         <tr>
